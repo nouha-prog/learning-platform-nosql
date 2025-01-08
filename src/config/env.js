57d89e0cf38d015f8f1422c497e6373f025044b8
nouha-prog <nouhailaelhajji488@gmail.com>
2025-01-08 10:24:43 +0100

    Reponse aux questions

--- src/config/env.js
+++ src/config/env.js
@@ -1,7 +1,16 @@
 // Question: Pourquoi est-il important de valider les variables d'environnement au démarrage ?
 // Réponse : 
+// La validation des variables d'environnement est importante car elle permet de s'assurer que 
+// toutes les informations nécessaires sont présentes avant de démarrer l'application.
+// Si une variable manquante empêche une connexion à la base de données ou d'autres services,
+// cela pourrait entraîner des erreurs imprévues en cours d'exécution. La validation garantit 
+// que l'application est correctement configurée avant son démarrage.
+
 // Question: Que se passe-t-il si une variable requise est manquante ?
-// Réponse : 
+// Réponse :
+// Si une variable d'environnement requise est manquante, l'application échouera lors du démarrage.
+// Cela peut entraîner des erreurs dans la logique métier, des connexions échouées à la base de données,
+// ou une défaillance dans la configuration d'un service, ce qui pourrait compromettre le bon fonctionnement de l'application.
 
 const dotenv = require('dotenv');
 dotenv.config();
